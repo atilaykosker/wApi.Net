75d475981f5207625b78a02d1e05c55551d0fbbb
atovarkosker <41235268+atovarkosker@users.noreply.github.com>
2020-11-30 00:59:52 +0300

    Creation of the general structure.

---
+++ DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer
+                (connectionString: @"Server=DESKTOP-AFN4BD5;Initial Catalog=Northwind; User ID=sa;Password=12345");
+            //<add name="Cnn2" connectionString="Data Source=DESKTOP-33NDNJ7\SQLEXPRESS;Initial Catalog=EsnafSeninleDB;User ID=sa;Password=B!arge2018" providerName="System.Data.SqlClient" />
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
